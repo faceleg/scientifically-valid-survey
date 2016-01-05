8309ad32619128793274b86cb4d27e608451472c
Michael Robinson <michael.robinson@solnet.co.nz>
2016-01-05 17:56:07 +1300

    Basic gulpfile bower and npm deps, config stub

---
+++ _config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MYSQL_CONNECTION="some connection string"
+TOKEN_SECRET="a very secret token ... secret"
+PORT=3030
+NODE_ENV="development"
+DEBUG="svs*"
---
+++ start-app.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f ./config.sh ]; then
+    echo "Please copy _config.sh and update its values to suit your environment"
+    exit 1
+fi
+
+source config.sh
+
+export MYSQL_CONNECTION=$MYSQL_CONNECTION
+export TOKEN_SECRET=$TOKEN_SECRET
+export PORT=$PORT
+export NODE_ENV=$NODE_ENV
+export DEBUG=$DEBUG
+
+# npm install --production
+# node_modules/.bin/bower install --production
+
+node_modules/.bin/gulp
+
+# node ./server/index.js

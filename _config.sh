4e9e210ff8f43baf5469a017fbc85099f87240af
Michael Robinson <mike@pagesofinterest.net>
2016-01-08 23:01:11 +1300

    Updated _config.sh to mirror new DB env vars

--- _config.sh
+++ _config.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-MYSQL_CONNECTION="mysql://root:password@localhost:3306/svs"
-TOKEN_SECRET="a very secret token ... secret"
-PORT=3030
 NODE_ENV="development"
+PORT=3030
+TOKEN_SECRET="a very secret token ... secret"
+
+RDS_USERNAME="svs"
+RDS_PASSWORD="svs"
+RDS_DB_NAME="svs"
+RDS_HOSTNAME="localhost"
+RDS_PORT=3306
+
 DEBUG="svs*"

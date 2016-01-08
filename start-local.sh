66c8707d46bc344c13efb6e7b6a8569a86f1c0ee
Michael Robinson <mike@pagesofinterest.net>
2016-01-08 23:00:17 +1300

    Moved start local script

--- start-local.sh
+++ start-local.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 if [ ! -f ./config.sh ]; then
     echo "Please copy _config.sh and update its values to suit your environment"
     exit 1
@@ -9,11 +7,16 @@ fi
 
 source config.sh
 
-export MYSQL_CONNECTION=$MYSQL_CONNECTION
 export TOKEN_SECRET=$TOKEN_SECRET
 export PORT=$PORT
 export NODE_ENV=$NODE_ENV
 export DEBUG=$DEBUG
+
+export RDS_USERNAME=$RDS_USERNAME
+export RDS_HOSTNAME=$RDS_HOSTNAME
+export RDS_PASSWORD=$RDS_PASSWORD
+export RDS_PORT=$RDS_PORT
+export RDS_DB_NAME=$RDS_DB_NAME
 
 npm install --production
 node_modules/.bin/bower install --production

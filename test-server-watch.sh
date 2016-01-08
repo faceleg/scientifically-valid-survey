f0437603577c694dfbfaf8523920854f04c24175
Michael Robinson <mike@pagesofinterest.net>
2016-01-09 01:53:57 +1300

    Updated server test scripts

--- test-server-watch.sh
+++ test-server-watch.sh
@@ -1,59 +1,10 @@
 #!/bin/bash
 
-# usage()
-# {
-# cat << EOF
-# usage: $0 options
+# connect-sequelize does not manage it's promises properly, resulting in a torrent of warning output
+export BLUEBIRD_W_FORGOTTEN_RETURN=0
 
-# This script runs tests and watch for changes
+mkdir -p coverage/server
 
-# OPTIONS:
-#    -c      required - database connection string
-#    -d      optional - debug string (defaults to "") use "*" to output verbose debug information
-#    -u      print usage and exit
-# EOF
-# }
-
-# MYSQL_CONNECTION=""
-# TOKEN_SECRET="TOKEN_SECRET"
-# DEBUG=
-# while getopts "c:d:" OPTION
-# do
-#      case $OPTION in
-#          c)
-#              MYSQL_CONNECTION=$OPTARG
-#              ;;
-#          d)
-#              DEBUG=$OPTARG
-#              ;;
-#          u)
-#              usage
-#              exit
-#              ;;
-#      esac
-# done
-
-# if [[ -z $MYSQL_CONNECTION ]]
-# then
-#      usage
-#      exit 1
-#   fi
-
-# export PORT=3031
-# export NODE_ENV="development"
-
-# export MYSQL_CONNECTION=$MYSQL_CONNECTION
-
-# export TOKEN_SECRET=$TOKEN_SECRET
-# export DEBUG=$DEBUG
-
-# mkdir -p coverage
-
-# User -g 'some string' to cause mocha to run only those tests with a matching 'it'
-# node_modules/.bin/istanbul cover --report html node_modules/.bin/_mocha -- -w --timeout 5000 --recursive --reporter spec test/server/configure.js test/server/**/*.js
-# node_modules/.bin/istanbul report text-summary > coverage/text-summary.txt
-# node_modules/.bin/coverage-average coverage/text-summary.txt --limit 95
-
-node_modules/.bin/istanbul cover --dir coverage/server node_modules/.bin/_mocha -- -w --timeout 5000 --recursive --reporter spec test/server/configure.js test/server/**/*.js
+node_modules/.bin/istanbul cover --dir coverage/server node_modules/.bin/_mocha -- -w --timeout 5000 --recursive --reporter spec test/server/configure.js test/server/
 node_modules/.bin/istanbul report text-summary > coverage/server/text-summary.txt
 node_modules/.bin/coverage-average coverage/text-summary.txt --limit 95
--- test-server.sh
+++ test-server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# connect-sequelize does not manage it's promises properly, resulting in a torrent of warning output
+export BLUEBIRD_W_FORGOTTEN_RETURN=0
+
 mkdir -p coverage/server
 
 node_modules/.bin/istanbul cover --dir coverage/server node_modules/.bin/_mocha -- --timeout 5000 --recursive --reporter spec test/server/configure.js test/server/**/*.js
